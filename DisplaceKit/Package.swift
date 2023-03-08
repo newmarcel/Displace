4c967d8f9691da008e249bc0fd6a18652d9687e4
Marcel Dierkes <marcel@dierkes.co>
2023-03-08 09:06:20 +0100

    Raised deployment target to macOS 12

--- DisplaceKit/Package.swift
+++ DisplaceKit/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "DisplaceKit",
     platforms: [
-        .macOS(.v11)
+        .macOS(.v12)
     ],
     products: [
         .library(name: "DisplaceKit", type: .dynamic, targets: ["DisplaceKit"]),
